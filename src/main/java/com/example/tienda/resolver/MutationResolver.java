0edd2dcd124ac617b9508728456b3866a1a6610f
yorman-129 <yorman_tobon82201@elpoli.edu.co>
2023-06-08 23:41:16 -0500

    First commit

---
+++ src/main/java/com/example/tienda/resolver/MutationResolver.java
@@ -0,0 +1,35 @@
+package com.example.tienda.resolver;
+
+
+import com.coxautodev.graphql.tools.GraphQLMutationResolver;
+import com.example.tienda.model.Cajero;
+import com.example.tienda.model.Producto;
+import com.example.tienda.repository.CajeroRepository;
+import com.example.tienda.repository.ProductoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.concurrent.ForkJoinPool;
+import java.util.concurrent.RecursiveTask;
+
+
+public class MutationResolver implements GraphQLMutationResolver {
+
+
+    @Autowired
+    private CajeroRepository cajeroRepository;
+    @Autowired
+    private ProductoRepository productoRepository;
+
+    public Cajero saveCajero(String nombreEmpleado){
+        Cajero cajero = Cajero.builder().nombreEmpleado(nombreEmpleado).build();
+        cajeroRepository.save(cajero);
+        return cajero;
+    }
+
+    public Producto saveProducto(String nombre, Integer cantidad){
+        Producto producto= Producto.builder().nombre(nombre).cantidad(cantidad).build();
+         productoRepository.save(producto);
+        return producto;
+    }
+
+}
